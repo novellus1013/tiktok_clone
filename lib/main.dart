8ecdd5f47ff29f5d7fc16d2c32f95712a2b16a4a
novellus1013 <novellus1013@gmail.com>
2024-10-11 15:40:58 +0900

    Constants and basic settings

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435a),
+        useMaterial3: true,
+      ),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: const Row(
+            children: [
+              Text("hello"),
+              Gaps.h20,
+              Text("hello"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
