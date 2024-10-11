90170678e69985506b8340bdbf88ea369f4b2fe4
novellus1013 <novellus1013@gmail.com>
2024-10-12 00:22:13 +0900

    Textfield with controller(TextEditController), AnimatedContainer

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class EmailScreen extends StatelessWidget {
   const EmailScreen({super.key});
@@ -8,7 +10,33 @@ class EmailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          "Sign up",
+          "EmailScreen",
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            Text(
+              "Create username",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            Text(
+              "You can always change this later.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
+import 'package:tiktok_clone/features/authentication/username_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -46,7 +46,7 @@ class SignUpScreen extends StatelessWidget {
               ),
               Gaps.v20,
               AuthButton(
-                screen: EmailScreen(),
+                screen: UsernameScreen(),
                 icon: FaIcon(FontAwesomeIcons.user),
                 text: "Use email and password",
               ),
