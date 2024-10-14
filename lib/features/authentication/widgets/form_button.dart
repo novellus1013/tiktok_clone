bd3ee1c0a8c354eeec052cf07e604f12a7909c1d
novellus1013 <novellus1013@gmail.com>
2024-10-14 23:32:22 +0900

    codechallenge add FormButton's value

--- lib/features/authentication/widgets/form_button.dart
+++ lib/features/authentication/widgets/form_button.dart
@@ -5,9 +5,11 @@ class FormButton extends StatelessWidget {
   const FormButton({
     super.key,
     required this.disabled,
+    required this.buttonText,
   });
 
   final bool disabled;
+  final String buttonText;
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +36,8 @@ class FormButton extends StatelessWidget {
             color: disabled ? Colors.grey.shade400 : Colors.white,
             fontWeight: FontWeight.w600,
           ),
-          child: const Text(
-            'Next',
+          child: Text(
+            buttonText,
             textAlign: TextAlign.center,
           ),
         ),
