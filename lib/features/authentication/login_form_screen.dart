2f232e08f075dd83e3664793f332792c916b04c9
novellus1013 <novellus1013@gmail.com>
2024-10-15 01:14:19 +0900

    Form widgets and GlobalKey to handle FormState

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
@@ -12,6 +13,11 @@ class LoginFormScreen extends StatefulWidget {
 
 class _LoginFormScreenState extends State<LoginFormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordCotroller = TextEditingController();
+
+  Map<String, String> formData = {};
+
+  bool _obscureText = true;
 
   void _onSubmitTap() {
     //validator에 에러가 없다면 true를 반환.
@@ -37,64 +43,103 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     return regExp.hasMatch(value!) ? true : false;
   }
 
-  Map<String, String> formData = {};
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onClearTap() {
+    _passwordCotroller.clear();
+  }
+
+  void _toggleObscureText() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Log in"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Log in"),
         ),
-        //반드시 key 값을 추가해 주어야 currentState가 변경됨
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Gaps.v28,
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Email",
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          //반드시 key 값을 추가해 주어야 currentState가 변경됨
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Gaps.v28,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                  ),
+                  validator: (value) {
+                    return _emailValidator(value) ? null : "Email not valid";
+                  },
+                  //저장된 순간의 입력값 = newValue;
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['email'] = newValue;
+                    }
+                  },
                 ),
-                validator: (value) {
-                  return _emailValidator(value) ? null : "Email not valid";
-                },
-                //저장된 순간의 입력값 = newValue;
-                onSaved: (newValue) {
-                  if (newValue != null) {
-                    formData['email'] = newValue;
-                  }
-                },
-              ),
-              Gaps.v16,
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: "Password",
+                Gaps.v16,
+                TextFormField(
+                  controller: _passwordCotroller,
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      suffixIcon: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          GestureDetector(
+                            onTap: _onClearTap,
+                            child: FaIcon(
+                              FontAwesomeIcons.solidCircleXmark,
+                              color: Colors.grey.shade500,
+                              size: Sizes.size20,
+                            ),
+                          ),
+                          Gaps.h16,
+                          GestureDetector(
+                            onTap: _toggleObscureText,
+                            child: FaIcon(
+                              _obscureText
+                                  ? FontAwesomeIcons.eyeSlash
+                                  : FontAwesomeIcons.eye,
+                              color: Colors.grey.shade500,
+                              size: Sizes.size20,
+                            ),
+                          ),
+                        ],
+                      )),
+                  validator: (value) {
+                    return _passwordValidator(value)
+                        ? null
+                        : "Password not valid";
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['password'] = newValue;
+                    }
+                  },
                 ),
-                validator: (value) {
-                  return _passwordValidator(value)
-                      ? null
-                      : "Password not valid";
-                },
-                onSaved: (newValue) {
-                  if (newValue != null) {
-                    formData['password'] = newValue;
-                  }
-                },
-              ),
-              Gaps.v28,
-              GestureDetector(
-                onTap: _onSubmitTap,
-                child: const FormButton(
-                  disabled: false,
-                  buttonText: 'Login',
+                Gaps.v28,
+                GestureDetector(
+                  onTap: _onSubmitTap,
+                  child: const FormButton(
+                    disabled: false,
+                    buttonText: 'Login',
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
